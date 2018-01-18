9ad1c07e2569c987744b117a1ac97b1c91789ce2
Monika Markowska <monika.nesteruk@gmail.com>
2018-01-18 09:52:41 +0100

    Div- dodany za pomaca jQuery

--- zadania-domowe/zadanie-19/js/pobierz-dane-programisty.js
+++ zadania-domowe/zadanie-19/js/pobierz-dane-programisty.js
@@ -7,14 +7,11 @@ $(function () {
 
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
 
-            $('#dane-programisty').html("");
-
-            $('#dane-programisty').append('<p>' + data.imie + " " + data.nazwisko + '</p>' + '<p>' + data.zawod + '</p>' + '<p>' + data.firma + '</p>');
+            $('p').text("");
+            $('#przycisk').after('<div>' + '<p>' + data.imie + " " + data.nazwisko + '</p>' + '<p>' + data.zawod + '</p>' + '<p>' + data.firma + '</p>' + '</div>');
 
         });
     });
-
-
 
 
 });
